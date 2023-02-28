c4fcceb114aadcde2bf9c5f9ebd70c3aef2a1f55
Junita Berglin <junita.berglin@gmail.com>
2023-02-28 13:57:07 +0100

    controllers continuing

--- src/controllers/storeController.js
+++ src/controllers/storeController.js
@@ -1,26 +1,23 @@
 const { QueryTypes } = require("sequelize");
-const { user } = require("../data/users");
-const { store } = require("../data/stores");
+const { userRoles } = require("../constants/users");
 const { sequelize } = require("../database/config");
 const { UnauthorizedError } = require("../utils/errors");
 
 exports.getAllStores = (req, res) => res.send("getAllStores");
-exports.getTodoById = (req, res) => {
+exports.getStoreById = (req, res) => {
   return res.send("getStoreById");
 };
 
 exports.createNewStore = async (req, res) => {
   const store = req.body.store;
-  const listId = req.params.storeId || req.body.storeId;
+  const listId = req.params.listId || req.body.listId;
 
-  if (req.user.ADMIN !== user.ADMIN) {
-    const [userListRole, userMeta] = await sequelize.query(
+  if (req.user.role !== userRoles.ADMIN) {
+    const [userRole, userRoleMeta] = await sequelize.query(
       `
-        SELECT r.role_name 
-        FROM users_lists ul
-          JOIN roles r ON r.id = ul.fk_roles_id 
-        WHERE ul.fk_lists_id = $listId AND fk_users_id = $userId 
-        LIMIT 1
+      INSERT _STORE =
+      INSERT INTO stores (store_name, store_description, store_adress, store_zipcode, store_city, store_createdBy_fk_user_id)
+      VALUES (store_name, store_description, store_adress, store_zipcode, store_city, store_createdBy_fk_user_id)
       `,
       {
         bind: { listId: listId, userId: req.user.userId },
@@ -28,7 +25,7 @@ exports.createNewStore = async (req, res) => {
       }
     );
 
-    if (!user.ADMIN) {
+    if (!userRole) {
       throw new UnauthorizedError(
         "You are not allowed to perform this action"
       );
@@ -36,7 +33,7 @@ exports.createNewStore = async (req, res) => {
   }
 
   const [newStoreId] = await sequelize.query(
-    "INSERT INTO Stores (store, fk_review_id) VALUES ($store, $listId);",
+    "INSERT INTO getAllStores (store, fk_lists_id) VALUES ($store, $listId);",
     {
       bind: { store: store, listId: listId },
       type: QueryTypes.INSERT, // returns ID of created row
