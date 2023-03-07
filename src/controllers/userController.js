5de2870fd5f3fbcfec5449fa02d30f5f9c365956
Ida Lindgren <ida.nacka@gmail.com>
2023-03-07 10:03:58 +0100

    update userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,21 +1,13 @@
-<<<<<<< HEAD
  // kunna skapa user och hämta alla user, hämta user baserat på id, ta bort id (usern själv och admin)
-const { user } = require("../data/users");
-=======
->>>>>>> d31e64b8ea683d768b74a26f9517bb21c2c6ea09
+const { users } = require("../data/users");
 const { NotFoundError, UnauthorizedError } = require("../utils/errors");
 const { sequelize } = require("../database/config");
 const { QueryTypes } = require("sequelize");
 
-<<<<<<< HEAD
-exports.getAllUsers = async (req, res) => { //admin 
+exports.getAllUsers = async (req, res) => {
   const [users, metadata] = await sequelize.query(
     "SELECT username, password, email FROM users"
   );
-=======
-exports.getAllUsers = async (req, res) => {
-  const [users, metadata] = await sequelize.query("SELECT * FROM user");
->>>>>>> d31e64b8ea683d768b74a26f9517bb21c2c6ea09
   return res.json(users);
 };
 
@@ -23,9 +15,9 @@ exports.getUserById = async (req, res) => {
   const userId = req.params.userId;
 
   const [user, metadata] = await sequelize.query(
-    "SELECT id, email FROM user WHERE id = $userId",
+    "SELECT user_id, email, name FROM users WHERE user_id = $userId",
     {
-      bind: { userId: userId },
+      bind: { userId },
       type: QueryTypes.SELECT,
     }
   );
@@ -37,32 +29,45 @@ exports.getUserById = async (req, res) => {
 
 exports.deleteUserById = async (req, res) => {
   const userId = req.params.userId;
+  console.log(userId);
 
-<<<<<<< HEAD
-  if (
-    userId != req.user?.userId &&
-    req.user.role !== userRoles.ADMIN
-  ) {
-=======
-  // Check if user is admin || user is requesting to delete themselves
-  if (userId != req.user?.userId && req.user.role !== userRoles.ADMIN) {
->>>>>>> d31e64b8ea683d768b74a26f9517bb21c2c6ea09
-    throw new UnauthorizedError("Unauthorized Access");
-  }
+  if ( req.user.role == userRoles.ADMIN || req.user.userId == userId ) {
+    // userId != req.user?.userId &&
+    // req.user.role !== userRoles.ADMIN
+  // ) {
+  //   throw new UnauthorizedError("Unauthorized Access");
+  // }
 
-  const [results, metadata] = await sequelize.query(
-    "DELETE FROM user WHERE id = $userId RETURNING *",
+  const [user, metadata] = await sequelize.query(
+    `DELETE FROM review WHERE fk_user_id = $userId;
+     DELETE FROM user WHERE user_id = $userId;`,
     {
-      bind: { userId: userId },
+      bind: { userId },
+      type: QueryTypes.DELETE,
     }
   );
 
-  if (!results || !results[0])
+  if (!user) {
     throw new NotFoundError("That user does not exist");
-
-  await sequelize.query("DELETE FROM user_lists WHERE fk_user_id = $userId", {
-    bind: { userId: userId },
-  });
-
+  }
+} else {
+    throw new UnauthorizedError("You don't have permission to delete this user");
+  }
   return res.sendStatus(204);
 };
+
+  // await sequelize.query(
+  //   "DELETE FROM users_lists WHERE fk_usersid = $userId",
+  //   {
+  //     bind: { userId },
+  //   }
+  // );
+
+  exports.createNewUser = async (req, res) => {
+    return res.send("createNewUser");
+  };
+
+  exports.updateUserById = async (req, res) => {
+    return res.send("updateUserById")
+  }
+
